93ab76085c5084a3e1b48b32d9835ed9eb2f3af0
Hyuan Peixoto <hyuanpeixoto@gmail.com>
2021-02-13 21:14:11 -0300

    did 5 trails

--- Assets/Scripts/AudioSyncEngine/AudioSyncSpawner.cs
+++ Assets/Scripts/AudioSyncEngine/AudioSyncSpawner.cs
@@ -6,20 +6,25 @@ public class AudioSyncSpawner : AudioSyncer
 {
 
     public GameObject enemyObject;
+
+    [SerializeField]
+    private GameObject movingDolly;
+
+    [SerializeField]
+    private GameObject[] trailsObject = { }; 
     float[] Ypos = new float[] { 3f, 0, -3F };
     [SerializeField]
     float enemyX = 0;
-    float enemyY = 0;
     float enemyZ = 2f;
-
-
 
     public override void OnBeat()
     {
         base.OnBeat();
-        enemyY = Ypos[Random.Range(0, 3)];
-        Instantiate(enemyObject, new Vector3(transform.position.x, enemyY, enemyZ), transform.rotation);
+        var enemyY = trailsObject[Random.Range(0, trailsObject.Length)].transform.position.y;
+        var newEnemyObject = Instantiate(enemyObject, this.transform);
+        newEnemyObject.transform.position = new Vector3(movingDolly.transform.position.x + enemyX, enemyY, enemyZ);
     }
+
 
 
 }
