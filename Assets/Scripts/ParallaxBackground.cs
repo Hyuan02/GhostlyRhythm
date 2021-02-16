8a04680ecdd789fdc16fb0f92cd8c6173f8de9c1
Hyuan Peixoto <hyuanpeixoto@gmail.com>
2021-02-16 08:57:48 -0300

    new menu scene

--- Assets/Scripts/ParallaxBackground.cs
+++ Assets/Scripts/ParallaxBackground.cs
@@ -15,11 +15,11 @@ public class ParallaxBackground : MonoBehaviour
 
     }
 
-    void FixedUpdate()
+    void Update()
     {
     	float temp = (cam.transform.position.x * (1 - parallaxEffect));
         float dist = (cam.transform.position.x * parallaxEffect);
-        Debug.Log("StartPost = " + startpos + "| Temp =  "+ temp +"| Lenght = "+ length);
+
         transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
 
         if (temp > (startpos + length - 8.9f)) startpos += length - (8.9f * 2);
