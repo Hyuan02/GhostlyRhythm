2f4ae29291bc7e6cc438170e668663624cc364b7
Hyuan Peixoto <hyuanpeixoto@gmail.com>
2021-02-25 22:37:19 -0300

    made Video ending

---
+++ Assets/Scripts/EndGameMovie.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(VideoPlayer))]
+public class EndGameMovie : MonoBehaviour
+{
+    VideoPlayer actualPlayer;
+    bool activated = false;
+    private void Start()
+    {
+        actualPlayer = GetComponent<VideoPlayer>();
+        
+    }
+
+    private void Update()
+    {
+        if(actualPlayer.time >= actualPlayer.clip.length)
+        {
+            activated = true;
+            ChangeToInitScene();
+        }
+    }
+
+    private void ChangeToInitScene()
+    {
+        SceneManager.LoadSceneAsync(0, LoadSceneMode.Single);
+    }
+
+}
---
+++ Assets/Scripts/EndGameWatcher.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Video;
+
+public class EndGameWatcher : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource source;
+
+    [SerializeField]
+    private UnityEvent eventOnEnd;
+
+    bool activated = false;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if(source.time >= source.clip.length && !activated)
+        {
+            eventOnEnd.Invoke();
+            activated = true;
+        }
+    }
+
+
+}
